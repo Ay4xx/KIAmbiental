cb625602722a9f1aff62738cc47ada98723aa426
josehernandezvergara <127722062+josehernandezvergara@users.noreply.github.com>
2025-05-15 17:41:20 -0600

    backend con login/registro y hashing de contras

--- src/components/LoadingSpinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-//separamos el componente //lo comenten en dash solo para ver cambios
-const LoadingSpinner = () => (
-  <div role="status" aria-label="Cargando" className="flex justify-center items-center p-4">
-    <div className="animate-spin ... h-12 w-12 border-t-2 border-b-2 border-blue-500" />
-  </div>
-);
-
-export default LoadingSpinner;
